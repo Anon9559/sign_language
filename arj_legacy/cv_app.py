6b2f1eaacc36de02fea4921d5c2103b95eb7f553
mkomod <mkomodromos01@gmail.com>
2019-09-13 19:16:40 +0300

    removed some thing

--- arj_legacy/cv_app.py
+++ arj_legacy/cv_app.py
@@ -6,7 +6,7 @@ from keras.models import load_model
 from keras.preprocessing import image
 
 # import model
-constant_path = "./"
+constant_path = "./arj_legacy/"
 model = load_model('arj_model_3.h5')
 
 # Use Webcam
@@ -28,36 +28,28 @@ while True:
         print("Escape hit, closing...")
         break
     elif k % 256 == 32:
-        # SPACE pressed
-        img_name = "frame_{}.png".format(img_counter)
-        cv.imwrite(img_name, frame)
-        print("{} written".format(img_name))
-        img_counter += 1
-        
-        img_path = constant_path+str(img_name)
+        test_image = frame.copy()
+        test_image = cv.resize(test_image, (64, 64))
+        test_image = np.expand_dims(test_image, axis=0)
         
         
-        test_image.= image.load_img(img_path,target_size = (64,64))
-        test_image.= image.img_to_array(test_image.
-        test_image.= np.expand_dims(test_image.axis=0)
+        image_class = model.predict(test_image)
         
-        
-        image.class = model.predict(test_image.
-        
-        if image.class[0][0] ==1:
+        if image_class[0][0] == 1:
             print('Fist')
-        elif image.class[0][1] ==1:
+        elif image_class[0][1] == 1:
             print('Index')
-        elif image.class[0][2] ==1:
+        elif image_class[0][2] == 1:
             print('Loser')
-        elif image.class[0][3] ==1:
+        elif image_class[0][3] == 1:
             print('Okay')
-        elif image.class[0][4] ==1:
+        elif image_class[0][4] == 1:
             print('Open_5')
-        elif image.class[0][5] ==1:
+        elif image_class[0][5] == 1:
             print('Peace')
+        else:
+            print('not found')
             
-        os.remove(img_path)
 
 cam.release()
 
