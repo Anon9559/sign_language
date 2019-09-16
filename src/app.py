0a0c47eb5daad89c51ce6f6af42027cf8fbedacd
mkomod <mkomodromos01@gmail.com>
2019-09-17 01:19:13 +0300

    remove unused files

--- src/app.py
+++ src/app.py
@@ -2,44 +2,23 @@
 import cv2 as cv
 import numpy as np
 
-from defaults import font
-from defaults import gestures
-from keras.models import load_model
-from keras.preprocessing import image
+from settings import FONT
 from statistics import mean
 from time import time
 
-from utils.detect import load_inference_graph
-from utils.detect import detect_objects
 from utils.draw import bounding_box
 from utils.draw import crop
+
+from detector import Detector, MultiThreadedDetector
 
 NUM_FRAMES = 20
 fps_mean = 0
 fps_times = []
 
-model = load_model("./models/model_14_09.h5")
-
-def classify_gesture(frame):
-    pred_image = frame.copy()
-    cv.cvtColor(pred_image, cv.COLOR_BGR2RGB)
-    pred_image = cv.resize(pred_image, (64, 64))
-    pred_image = np.expand_dims(pred_image, axis=0)
-    
-    image_class = model.predict(pred_image)
-    try:
-        # finds index of classified gesture
-        gesture = gestures[image_class[0].tolist().index(1.0)]
-    except ValueError:
-        gesture = "NA"
-    
-    return gesture
-
-
 cam = cv.VideoCapture(0)
 cv.namedWindow("main")
 
-dg, sess = load_inference_graph()
+detector = Detector()
 
 while True:
     timer_start = time()
@@ -53,14 +32,16 @@ while True:
         print("no camera found")
         break
     
-    boxes, scores = detect_objects(cv.cvtColor(frame, cv.COLOR_BGR2RGB), dg, sess)
+    boxes, scores = detector.detect_objects(cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+    
+    # detect_objects(cv.cvtColor(frame, cv.COLOR_BGR2RGB), dg, sess)
     regions = crop(frame, boxes, scores, score=0.9)
     bounding_box(frame, boxes, scores)
 
-    gesture = classify_gesture(frame)
+    # gesture = classify_gesture(frame)
 
-    cv.putText(frame, gesture, (50, 32), font, 1, (255, 255, 255), 2, cv.LINE_AA)
-    cv.putText(frame, str(fps_mean), (10, 30), font, 0.7, (0, 255, 255), 2, cv.LINE_AA)
+    # cv.putText(frame, gesture, (50, 32), FONT, 1, (255, 255, 255), 2, cv.LINE_AA)
+    cv.putText(frame, str(fps_mean), (10, 30), FONT, 0.7, (0, 255, 255), 2, cv.LINE_AA)
     cv.imshow("main", frame)
 
     timer_end = time()
