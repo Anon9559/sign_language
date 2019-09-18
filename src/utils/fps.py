5fb6c528b1866e75354d8bdc2b4a4def98a0468a
mkomod <mkomodromos01@gmail.com>
2019-09-18 23:02:14 +0300

    docstring, fix

--- src/utils/fps.py
+++ src/utils/fps.py
@@ -4,14 +4,33 @@ import cv2 as cv
 from settings import FONT
 
 class Fps: 
-    # To measure the number of frame per second
+    """ Measures the fps (frames per second)
+    
+    Example;
 
+    >>> from utils.fps import Fps
+    
+    Setup
+
+    >>> cap = cv.videoCapture()
+    >>> fps = Fps()
+    
+    Main loop
+
+    >>> while True:
+    >>>     ret, frame = cap.read()
+    >>>     fps.update()
+    >>>     fps.display(frame)
+    >>>     cv.imshow("main", frame)
+    >>>     ...
+    """
     def __init__(self):
         self.nbf=0
         self.fps=0
         self.start=0
         
     def update(self):
+        """ Updates the fps reading """
         if self.nbf%10==0:
             if self.start != 0:
                 self.stop=time.time()
@@ -20,9 +39,21 @@ class Fps:
             else :
                 self.start=time.time()    
         self.nbf+=1
-    
-    def get(self):
-        return self.fps
 
-    def display(self, frame, orig=(10,30), font=FONT, size=0.7, color=(0,255, 255), thickness = 2):
-        cv.putText(frame, f"{self.get():.2f}", orig, font, size, color, thickness)
+    def display(self, frame, **kwargs):
+        """ Displays the fps on a given frame 
+        
+        Kwargs:
+          pos: (tuple) position of text
+          font: (int) font enumerator see cv2 fonts
+          size: (float) font size
+          colour: (tuple) BGR font colour
+          thickness: (float) text thickness
+        """
+        pos = kwargs.get("pos", (10,30))
+        font = kwargs.get("font", FONT)
+        size = kwargs.get("size", 0.7)
+        colour = kwargs.get("colour", (0, 255, 255))
+        thickness = kwargs.get("thickness", 2)
+
+        cv.putText(frame, f"{self.fps:.2f}", pos, font, size, colour, thickness)
