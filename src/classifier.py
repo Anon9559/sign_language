fa5213092cb620a29f14f1104bbb9961f9b84425
Anon9559 <zcbtask@ucl.ac.uk>
2019-10-06 21:04:22 +0100

    restoring basic functionality

--- src/classifier.py
+++ src/classifier.py
@@ -21,4 +21,16 @@ def classify_gesture(image):
         gesture = "NA"
     
     return gesture
-
+#####
+def classify_gesture_alt(image):
+    pred_image = image.copy()
+    pred_image = pred_image/255
+    pred_image = pred_image.reshape(-1,28,28,1)
+    image_class = model.predict(pred_image)
+    
+    try:
+        gesture = GESTURES[image_class[0].tolist().index(1.0)]
+    except ValueError:
+        gesture = "NA"
+    
+    return gesture
